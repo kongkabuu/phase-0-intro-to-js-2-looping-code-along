47a244e9c587021e6ed200efcf2d95f2d480a0ea
paul kabuu <kongkabuu9@gmail.com>
2023-03-11 22:52:43 +0300

    assingmnet done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, eventName){
+let eventsArray = [];
+for(let i = 0; i<array.length; i++){
+
+    eventsArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+}
+console.log(eventsArray);
+return eventsArray;
+}
+
+
+function countDown(x){
+while(x>=0){
+console.log(x);
+     x--;
+}
+}
